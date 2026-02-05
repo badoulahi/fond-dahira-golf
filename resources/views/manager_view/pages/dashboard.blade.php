b42893ce2efa12400ab5f772ee1282ca1be87776
alsow <badoulahi8@gmail.com>
2026-02-05 19:03:37 +0000

    Only Back

--- resources/views/manager_view/pages/dashboard.blade.php
+++ resources/views/manager_view/pages/dashboard.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
     <div class="page-inner">
-        <h3 class="fw-bold mb-3">Our Dashboard</h3>
+        <h3 class="fw-bold mb-3">Dashboard</h3>
 
         <div class="row">
             <div class="col-md-2">
--- routes/web.php
+++ routes/web.php
@@ -6,21 +6,21 @@ use App\Http\Controllers\Manager\ManagerController;
 use App\Http\Controllers\MembreController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [AccueilController::class, 'home'])->name('home');
+// Route::get('/', [AccueilController::class, 'home'])->name('home');
 
-Route::get('/about', [AccueilController::class, 'about'])->name('about');
+// Route::get('/about', [AccueilController::class, 'about'])->name('about');
 
-Route::get('/contact', [AccueilController::class, 'contact'])->name('contact');
+// Route::get('/contact', [AccueilController::class, 'contact'])->name('contact');
 
-Route::get('/clear-cache', function () {
-    Artisan::call('config:clear');
-    Artisan::call('cache:clear');
-    Artisan::call('route:clear');
-    Artisan::call('view:clear');
-    return "Cache cleared!";
-});
+// Route::get('/clear-cache', function () {
+//     Artisan::call('config:clear');
+//     Artisan::call('cache:clear');
+//     Artisan::call('route:clear');
+//     Artisan::call('view:clear');
+//     return "Cache cleared!";
+// });
 
-Route::get('/create', [ManagerController::class, 'create']);
+// Route::get('/create', [ManagerController::class, 'create']);
 
 Route::middleware(['guest:web'])->group(function () {
     Route::get('/login', [ManagerController::class, 'login'])->name('manager.login');
@@ -28,7 +28,7 @@ Route::middleware(['guest:web'])->group(function () {
 });
 
 Route::middleware(['auth:auth-manager'])->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'accueil'])->name('dashboard');
+    Route::get('/', [DashboardController::class, 'accueil'])->name('dashboard');
 
     Route::resource('membres', MembreController::class);
 
